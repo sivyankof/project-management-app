beca11a9025eaef80224b37b7bb56f872ff688d3
hokmyn <den_from_bykhov1988@mail.ru>
2022-05-03 11:46:39 +0300

    fix: cancel changes in package-lock

--- src/app/core/not-found/not-found.component.ts
+++ src/app/core/not-found/not-found.component.ts
@@ -1,10 +1,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-  selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-not-found',
+    templateUrl: './not-found.component.html',
+    styleUrls: ['./not-found.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NotFoundComponent {
-}
+export class NotFoundComponent {}
