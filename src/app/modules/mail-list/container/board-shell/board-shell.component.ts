597d2236e2a7706696ea725da574def9344f3f72
Artem Sivyankov <artemsivyankov@coherentsolutions.com>
2022-05-07 19:37:36 +0400

    feat: add board

---
+++ src/app/modules/mail-list/container/board-shell/board-shell.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-board-shell',
+    templateUrl: './board-shell.component.html',
+    styleUrls: ['./board-shell.component.scss'],
+})
+export class BoardShellComponent {}
